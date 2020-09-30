d85f898aca08827d073009b077ed442f68d22efb
yakov avraham <yakov.avra@gmail.com>
2020-10-01 01:23:45 +0300

    add 404 not found page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div style={{fontSize:"32px",textAlign:"center", alignContent:"center"}}>
+            404 - Page Not Found
+        </div>
+    )
+}
