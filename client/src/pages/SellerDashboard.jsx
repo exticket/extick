10a045cf86d1732c9daaa7243a78ecbb2fb4296c
yakov avraham <yakov.avra@gmail.com>
2020-10-01 02:46:25 +0300

    add "add new ticket button

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import LinkButton from '../components/LinkButton';
 
 export default function SellerDashboard() {
     return (
         <div>
-            SellerDashboard
+            <div className="seller-dashboard-header">
+                <h1>Your tickets:</h1>
+                <LinkButton text="ADD NEW TICKET" url="/sellers/selltickets" className="add-new-ticket" />
+            </div>
         </div>
     )
 }
