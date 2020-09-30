8de3efecc787f422b5741d20f82472c1e1e6e6ee
yakov avraham <yakov.avra@gmail.com>
2020-10-01 01:32:44 +0300

    add 404 not found page (forgot to stage app.jsx)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import EventProfile from './components/EventProfile/EventProfile';
 import { Homepage, LoginPage, PublishTicket, SellerDashboard, TicketManagement, SignupPage, SellTickets, TicketDetails } from './pages';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NotFound from './pages/NotFound';
 
 
 
@@ -23,6 +24,7 @@ function App() {
                     <Route path="/sellers/ticket/:id/management" exact component={TicketManagement} />
                     <Route path="/sellers/SellTickets" exact component={SellTickets} />
                     <Route path="/eventProfile" exact component={EventProfile} />
+                    <Route component={NotFound} />
                 </Switch>
             </Router>
         </main>
