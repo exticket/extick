5a29c2c96dc910f487e012e93a1dfad6a8dcd010
hagay kassa <hagaykassa@gmail.com>
2020-10-08 15:05:33 +0300

    changing login-api to authentication-api

--- client/src/apis/authentication-Api.js
+++ client/src/apis/authentication-Api.js
@@ -2,13 +2,17 @@ import axios from 'axios';
 import backendUrl from './backend-url';
 
 const api = axios.create({
-    baseURL:`${backendUrl}/login`
+    baseURL:`${backendUrl}/authentication`
 })
 export async function loginRequest(email, password) {
-    const resp = await api.post("/", {
+    const resp = await api.post("/login", {
         email,
         password
     });
 
     return resp;
+}
+export async function logoutRequest() {
+    const res = await api.get('/logout');
+    return res.message;
 }
