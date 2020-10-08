75cfde2668394829c5de4726c11405dcd6a3aed3
hagay kassa <hagaykassa@gmail.com>
2020-10-08 15:50:06 +0300

    adding function that connect logout button to the logout function in the server side

--- client/src/components/HelloMsg.jsx
+++ client/src/components/HelloMsg.jsx
@@ -1,12 +1,19 @@
 import React, {useContext} from 'react';
 import SellerContext from '../SellerContext';
+import { logoutRequest } from '../apis/authentication-Api';
 
 export default function HelloMsg({seller}) {
     const logOutFunction = useContext(SellerContext).logOut;
+
+    function logOut(){
+       logOutFunction();
+        logoutRequest();
+
+    }
     return (
         <div className="header-log-area logout-btn">
             <span>{`Hello ${seller.first_name},`}</span>
-            <button onClick={() => logOutFunction()}>Log Out</button>
+            <button onClick={() => logOut()}>Log Out</button>
         </div>
     )
 }
