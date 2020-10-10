83879ce34819acb8bb374aa31acb3f18dc353400
hagay kassa <hagaykassa@gmail.com>
2020-10-10 22:27:55 +0300

    fixing delete function in ticket-ctrl

--- controllers/ticket-ctrl.js
+++ controllers/ticket-ctrl.js
@@ -82,14 +82,15 @@ const updateTicket = (req, res) => {
             })
         })
 }
-const deleteTicket = (req, res) => {
-    Ticket.findOneAndDelete({ _id: req.params.id }).then(found => {
-    if (!found) {
-        return res.status(404).json({ success: false, error: "There is no ticket with the given id" })
-    }
-    res.status(200).json({ success: true, data: found._doc })
-}).catch(err=> res.status(400).json({ success: false, error: err.message }))
+
+const deleteTicket = (req,res)=>{
+    Ticket.findOneAndDelete({ _id:req.params.id}).then(ticket =>{
+        if(!ticket){
+        return res.status(400).json({success:false,error:'Ticket not found'})}
+        return res.status(200).json({success:true,data:ticket})  
+    }).catch(err=>console.log(err))
 }
+
 const getTicketbyid = (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
         return res.status(400).json({ success: false, message: 'Id not valid' })
