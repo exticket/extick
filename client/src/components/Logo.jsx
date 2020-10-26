0f9a8f9e49a251e7ac10d4aaae371fa30fb45293
yakov avraham <yakov.avra@gmail.com>
2020-10-26 15:49:58 +0200

    set logo path reletive to public folder

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 
 export default function Logo() {
-    return <Link  to="/"><img title="GO TO HOMEPAGE" src="logo.png" alt="Extick logo" width="140" height="35"/></Link>
+    return <Link  to="/"><img title="GO TO HOMEPAGE" src="/logo.png" alt="Extick logo" width="140" height="35"/></Link>
 }
