532716b7abad5dc68623515777dac6a8c6deb712
yakov avraham <yakov.avra@gmail.com>
2020-10-26 15:01:15 +0200

    remove circle element in card component

--- client/src/components/TicketsContainer/CardComponent.jsx
+++ client/src/components/TicketsContainer/CardComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './CardComponent.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import ModifyTicketIcons from '../ModifyTicketIcons';
 
 
@@ -12,35 +12,26 @@ export default function CardComponent({ eventInfo, onTicketDeleted }) {
     const date = dateObject.toLocaleDateString();
     const time = dateObject.toLocaleTimeString();
 
-    const onCardClick = (eventInfo) => {
-        console.log(eventInfo);
-        history.push({
-            pathname: '/eventProfile',
-            props: { eventInfo }
-        });
-    }
-
     return (
+        <Link className="card-link" to={`/eventProfile/${eventInfo._id}`}>
+            <div title="Click For Details" className="card ticket">
+                <div className="eventImgContainer">
+                    <img className="eventImg" src={eventInfo.imgUrl} alt="event" />
+                    <div className="card-header">
+                        <span className="eventName">{eventInfo.ticket_title}</span>
+                    </div>
+                </div>
 
-        <div title="Click For Details" className="card ticket" onClick={() => onCardClick(eventInfo)}>
-            <div className="eventImgContainer">
-                <img className="eventImg" src={eventInfo.imgUrl} alt="event" />
-                <div className="card-header">
-                    <div className="circle-icon"></div>
-                    <span className="eventName">{eventInfo.ticket_title}</span>
+                <div className="card-content">
+                    <p className="date">{date} </p>
+
+                    <p className="hour">{time}</p>
+                    <p className="location">{eventInfo.location}</p>
+                    <p className="price">{eventInfo.price} ₪</p>
+                    {history.location.pathname === '/sellers/mytickets' && <ModifyTicketIcons ticketId={eventInfo._id} onTicketDeleted={onTicketDeleted} />}
                 </div>
             </div>
-
-            <div className="card-content">
-                <p className="date">{date} </p>
-            
-                <p className="hour">{time}</p>
-                <p className="location">{eventInfo.location}</p>
-                {/* <p className="sellerName">{eventInfo.seller_Id}</p> */}
-                <p className="price">{eventInfo.price} ₪</p>
-                {history.location.pathname === '/sellers/mytickets' && <ModifyTicketIcons ticketId={eventInfo._id} onTicketDeleted={onTicketDeleted}/>}
-            </div>
-        </div>
+        </Link>
     )
 }
 
