4b71a0b40f99288dbdca1ed47d2d5455e5d8952b
yakov avraham <yakov.avra@gmail.com>
2020-10-27 02:19:20 +0200

    sellers can edit their account details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import EventProfile from './components/EventProfile/EventProfile';
-import { Homepage, LoginPage, PublishTicket, SellerMyTickets, TicketManagement, SignupPage, SellTickets, LoginSignupPage } from './pages';
+import { Homepage, LoginPage, PublishTicket, SellerMyTickets, TicketManagement, SignupPage, SellTickets, LoginSignupPage, MyAccount } from './pages';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NotFound from './pages/NotFound';
@@ -23,6 +23,7 @@ function App() {
                         <Route path="/sellers/selltickets/login" exact component={LoginSignupPage} />
                         <Route path="/sellers/signup" exact component={SignupPage} />
                         <Route path="/sellers/mytickets" exact component={WithLoginRequired(SellerMyTickets)} />
+                        <Route path="/sellers/myaccount" exact component={WithLoginRequired(MyAccount)} />
                         <Route path="/sellers/publish-ticket" exact component={WithLoginRequired(PublishTicket)} />
                         <Route path="/ticket/management/:id" exact component={TicketManagement} />
                         <Route path="/sellers/SellTickets" exact component={(SellTickets)} />
