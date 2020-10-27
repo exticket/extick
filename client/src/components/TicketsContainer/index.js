c8a2f05f492e4976b12ca3c01d1a1351234e0057
yakov avraham <yakov.avra@gmail.com>
2020-10-28 01:45:20 +0200

    add search bar for search tickets

--- client/src/components/TicketsContainer/index.js
+++ client/src/components/TicketsContainer/index.js
@@ -3,24 +3,42 @@ import CardComponent from './CardComponent';
 import { Link, useLocation } from 'react-router-dom';
 import AddCardComponent from './AddCardComponent';
 import OutOfStock from '../Home/OutOfStock';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function TicketsContainer({ tickets, categoryId, onTicketDeleted }) {
 
     const [currentTickets, setCurrentTickets] = useState([]);
     const pathname = useLocation().pathname;
+    const [searchValue, setSearchValue] = useState("");
+
+    function filterTickets(disableSearch) {
+        setCurrentTickets(tickets.filter((ticket) => {
+            return !categoryId || ticket.category_Id._id === categoryId;
+        }).filter(ticket => disableSearch || !searchValue || ticket.ticket_title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+        )
+    }
 
     useEffect(() => {
-        setCurrentTickets(tickets.filter((ticket) => {
-
-            return !categoryId || ticket.category_Id === categoryId;
-        })
-        )
+        setSearchValue("");
+        filterTickets(true);
     }, [categoryId, tickets]);
+
+    function searchHandler() {
+        filterTickets();
+    }
 
     return (
         <div className="tickets-container">
+            <div className="search-bar">
+                <TextField className="search-input" value={searchValue} onChange={event => setSearchValue(event.target.value)} label="Search" variant="outlined" />
+                <IconButton onClick={searchHandler}>
+                    <SearchIcon />
+                </IconButton>
+            </div>
             {pathname === '/sellers/mytickets' && <Link to="/sellers/selltickets"><AddCardComponent /></Link>}
-            { categoryId && currentTickets.length === 0 ? <OutOfStock/>: currentTickets.map((ticket) => <CardComponent key={ticket._id} eventInfo={ticket} onTicketDeleted={onTicketDeleted} />)}
+            { categoryId && currentTickets.length === 0 ? <OutOfStock /> : currentTickets.map((ticket) => <CardComponent key={ticket._id} eventInfo={ticket} onTicketDeleted={onTicketDeleted} />)}
         </div>
     )
 }
